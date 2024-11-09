9512c38a019b9356dcd25a0829520436b76358a3
Razen <09876sp54321@gmail.com>
2024-11-10 00:41:05 +0530

    added report functionality and minor interactions

--- src/components/Predict/Predict.jsx
+++ src/components/Predict/Predict.jsx
@@ -1,7 +1,67 @@
 // import React from 'react'
+import { useEffect, useState } from "react"
 import { motion } from "framer-motion"
+import symptomsList from "../../symptoms.json"
+import Report from "../Report/Report"
 
 function Predict() {
+
+    const [symptoms, setSymptoms] = useState([])
+    const [patientDetails, setPatientDetails] = useState({
+        "patientName": "",
+        "patientAge": "",
+        "patientGender": "",
+        "patientWeight": "",
+        "patientHeight": "",
+        "patientBloodGroup": "",
+        "symptoms": []
+    })
+    const [diseasePredicted, setDiseasePredicted] = useState({})
+
+    const addSymptom = () => {
+        setSymptoms([...symptoms, { id: Date.now(), value: '' }])
+    }
+
+    const handleSymptomChange = (event, index) => {
+        let updatedSymptoms = [...symptoms];
+        updatedSymptoms[index] = { ...updatedSymptoms[index], value: event.target.value };
+        setPatientDetails((prevDetails) => ({
+            ...prevDetails,
+            symptoms: updatedSymptoms
+        }))
+        setSymptoms(updatedSymptoms)
+    }
+
+    const handlePatientDetailsChange = (event) => {
+        let { name, value } = event.target
+        setPatientDetails((prevDetails) => ({
+            ...prevDetails,
+            [name]: value
+        }))
+    }
+
+    const predictDisease = async () => {
+        console.log(patientDetails.symptoms)
+        try {
+            const response = await fetch('http://localhost:3000/predict-disease', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ symptoms: patientDetails.symptoms })
+            })
+            const data = await response.json();
+            setDiseasePredicted(data)
+            console.log(data)
+        } catch (error) {
+            console.error("Error fetching prediction:", error)
+        }
+    }
+
+    useEffect(() => {
+        console.log(diseasePredicted)
+    }, [diseasePredicted])
+
     return (
         <div className="predict mt-24">
             <h1 className="text-5xl capitalize text-center">Discover What&apos;s Behind Your Symptoms</h1>
@@ -11,30 +71,77 @@ function Predict() {
 
                     <div className="info-input mt-10">
                         <h3 className="font-semibold">Name</h3>
-                        <input type="text" className="px-2 py-1 border-2 mt-2 w-full bg-gray-100 rounded" />
+                        <input
+                            type="text"
+                            name="patientName"
+                            value={patientDetails.patientName}
+                            className="px-2 py-1 min-w-28 border-2 mt-2 w-full bg-gray-100 rounded"
+                            onChange={(e) => handlePatientDetailsChange(e)}
+                        />
                         <div className="measurements mt-4 grid grid-cols-3 grid-flow-row gap-2">
                             <div>
                                 <h3 className="mb-4 font-semibold">Gender</h3>
-                                <select name="gender" id="gender" className="px-2 py-2">
+                                <select
+                                    name="patientGender"
+                                    id="patientGender"
+                                    value={patientDetails.patientGender}
+                                    onChange={(e) => handlePatientDetailsChange(e)}
+                                    className="px-2 py-2 min-w-28"
+                                >
+                                    <option value="" disabled>--Select--</option>
                                     <option value="Male">Male</option>
-                                    <option value="Male">Female</option>
+                                    <option value="Female">Female</option>
                                 </select>
                             </div>
                             <div>
                                 <h3 className="mb-2 font-semibold">Age</h3>
-                                <input type="number" className="px-2 py-1 border-2 mt-2 w-2/5 bg-gray-100 rounded" />
+                                <input
+                                    type="number"
+                                    name="patientAge"
+                                    value={patientDetails.patientAge}
+                                    onChange={(e) => handlePatientDetailsChange(e)}
+                                    className="px-2 py-1 min-w-28 border-2 mt-2 w-2/5 bg-gray-100 rounded"
+                                />
                             </div>
                             <div>
                                 <h3 className="mb-2 font-semibold">Height (in cm)</h3>
-                                <input type="number" className="px-2 py-1 border-2 mt-2 w-2/5 bg-gray-100 rounded" />
+                                <input
+                                    type="number"
+                                    name="patientHeight"
+                                    value={patientDetails.patientHeight}
+                                    onChange={(e) => handlePatientDetailsChange(e)}
+                                    className="px-2 py-1 min-w-28 border-2 mt-2 w-2/5 bg-gray-100 rounded"
+                                />
                             </div>
                             <div>
                                 <h3 className="mb-2 font-semibold">Weight (in Kg)</h3>
-                                <input type="number" className="px-2 py-1 border-2 mt-2 w-2/5 bg-gray-100 rounded" />
+                                <input
+                                    type="number"
+                                    name="patientWeight"
+                                    value={patientDetails.patientWeight}
+                                    onChange={(e) => handlePatientDetailsChange(e)}
+                                    className="px-2 py-1 min-w-28 border-2 mt-2 w-2/5 bg-gray-100 rounded"
+                                />
                             </div>
                             <div>
                                 <h3 className="mb-2 font-semibold">Blood Group</h3>
-                                <input type="number" className="px-2 py-1 border-2 mt-2 w-2/5 bg-gray-100 rounded" />
+                                <select
+                                    name="patientBloodGroup"
+                                    value={patientDetails.patientBloodGroup}
+                                    onChange={(e) => handlePatientDetailsChange(e)}
+                                    id="blood-group"
+                                    className="px-2 py-2 min-w-28"
+                                >
+                                    <option value="" disabled>--Select--</option>
+                                    <option value="A+">A+</option>
+                                    <option value="A-">A-</option>
+                                    <option value="B+">B+</option>
+                                    <option value="B-">B-</option>
+                                    <option value="AB+">AB+</option>
+                                    <option value="AB-">AB-</option>
+                                    <option value="O+">O+</option>
+                                    <option value="O-">O-</option>
+                                </select>
                             </div>
                         </div>
                         <motion.button
@@ -45,6 +152,7 @@ function Predict() {
                                 },
                             }}
                             whileTap={{ scale: 0.9 }}
+                            onClick={predictDisease}
                             className="text-xl mt-12 px-8 py-4 mr-8 bg-black text-white rounded"
                         >
                             Predict Now
@@ -52,50 +160,41 @@ function Predict() {
                     </div>
                 </div>
 
-                <div className="symptoms mt-20 py-4 px-8 bg-gray-200 rounded overflow-scroll w-2/5">
+                <div className="symptoms mt-20 py-4 px-8 bg-gray-200 rounded w-2/5">
                     <h1>Add your Symptoms using the dropdown</h1>
-                    <div className="add-symptom mt-6">
-                        <h2 className="font-semibold mb-2">Symptom 1</h2>
-                        <select name="symptom" id="symptom1" className="px-2 py-2 bg-white w-full rounded">
-                            <option value="Sneeze">Sneeze</option>
-                            <option value="Sneeze">Cough</option>
-                            <option value="Sneeze">Rash</option>
-                            <option value="Sneeze">Difficult Breathing</option>
-                        </select>
+                    <div className="max-h-80 overflow-scroll">
+                        {symptoms.map((eachSymptom, index) => (
+                            <div className="add-symptom mt-6" key={eachSymptom.id}>
+                                <h2 className="font-semibold mb-2">Symptom {index + 1}</h2>
+                                <select
+                                    className="px-2 py-2 bg-white w-full rounded"
+                                    onChange={(e) => handleSymptomChange(e, index)}
+                                    value={eachSymptom.value}
+                                >
+                                    <option value="" disabled>
+                                        Select a symptom
+                                    </option>
+                                    {symptomsList.map((eachSymptomInTheList, idx) => (
+                                        <option value={eachSymptomInTheList} key={idx}>
+                                            {eachSymptomInTheList}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        ))}
                     </div>
-                    <div className="add-symptom mt-6">
-                        <h2 className="font-semibold mb-2">Symptom 2</h2>
-                        <select name="symptom" id="symptom1" className="px-2 py-2 bg-white w-full rounded">
-                            <option value="Sneeze">Sneeze</option>
-                            <option value="Sneeze">Cough</option>
-                            <option value="Sneeze">Rash</option>
-                            <option value="Sneeze">Difficult Breathing</option>
-                        </select>
-                    </div>
-                    <div className="add-symptom mt-6">
-                        <h2 className="font-semibold mb-2">Symptom 3</h2>
-                        <select name="symptom" id="symptom1" className="px-2 py-2 bg-white w-full rounded">
-                            <option value="Sneeze">Sneeze</option>
-                            <option value="Sneeze">Cough</option>
-                            <option value="Sneeze">Rash</option>
-                            <option value="Sneeze">Difficult Breathing</option>
-                        </select>
-                    </div>
+
                     <motion.button
-                        whileHover={{
-                            scale: 1.1,
-                            transition: {
-                                duration: 0.2,
-                            },
-                        }}
+                        whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
                         whileTap={{ scale: 0.9 }}
                         className="mt-10 px-8 py-4 bg-black text-white rounded"
+                        onClick={addSymptom}
                     >
                         Add more symptoms
                     </motion.button>
                 </div>
             </div>
-
+            {diseasePredicted.predictions && <Report patientDetails={patientDetails} diseasePredicted={diseasePredicted}/>}
         </div>
     )
 }
---
+++ src/components/Report/Report.jsx
@@ -0,0 +1,67 @@
+import aiDoctorImage from "../../assets/stethescope.svg"
+
+const Report = ({ patientDetails, diseasePredicted }) => {
+
+    return (
+        <div className="mt-20 border-2 p-8">
+            <h1 className="text-3xl text-center underline">Report</h1>
+            <div className="report mt-10">
+                <div className="personal-details flex justify-between items-center">
+                    <div>
+                        <div className="flex">
+                            <h2 className="font-bold mr-2">Name: </h2>
+                            <p>{patientDetails.patientName}</p>
+                        </div>
+                        <div className="flex">
+                            <h2 className="font-bold mr-2">Gender: </h2>
+                            <p>{patientDetails.patientGender}</p>
+                        </div>
+                        <div className="flex">
+                            <h2 className="font-bold mr-2">Age: </h2>
+                            <p>{patientDetails.patientAge}</p>
+                        </div>
+                        <div className="flex">
+                            <h2 className="font-bold mr-2">Blood Group: </h2>
+                            <p>{patientDetails.patientBloodGroup}</p>
+                        </div>
+                        <div className="flex">
+                            <h2 className="font-bold mr-2">Height: </h2>
+                            <p>{patientDetails.patientHeight} cm</p>
+                        </div>
+                        <div className="flex">
+                            <h2 className="font-bold mr-2">Weight: </h2>
+                            <p>{patientDetails.patientWeight} Kg</p>
+                        </div>
+                    </div>
+                    <img src={aiDoctorImage} alt="Logo" className="w-32" />
+                    
+                </div>
+                <div className="predicted-diseases mt-6">
+                    <h1>Your Top 5 Predicted Diseases:</h1>
+                    <ol className="list-decimal ml-6 mt-4">
+                        {diseasePredicted && diseasePredicted.predictions ? diseasePredicted.predictions.map((eachDisease) => (
+                            <div className="diseases" key={eachDisease.disease}>
+                                <li className="font-semibold">{eachDisease.disease} - <span className="font-light">Probability: {parseFloat(eachDisease.probability.toFixed(2))}% :</span></li>
+                                <p>{eachDisease.description}</p>
+                            </div>
+
+
+                        )) : null}
+                    </ol>
+                </div>
+                <div className="precautions mt-6">
+                    <h1 className="font-bold">Suggested Precautions to be taken: </h1>
+                    <ol className="list-decimal ml-6 mt-4">
+                        {diseasePredicted && diseasePredicted.precautions ? diseasePredicted.precautions.map((eachPrecautions, index) => (
+                            <div className="precautions" key={index}>
+                                <li className="">{eachPrecautions.charAt(0).toUpperCase() + eachPrecautions.slice(1)}</li>
+                            </div>
+                        )) : null}
+                    </ol>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Report
